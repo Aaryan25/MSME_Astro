6f3ae054e69233182b02e5935a65a44958151025
Mayur Jain <jainmayur654@gmail.com>
2024-10-09 19:10:15 +0530

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aaryan25.github.io/',
+  base: 'MSME_Astro',
   integrations: [tailwind()]
 });
